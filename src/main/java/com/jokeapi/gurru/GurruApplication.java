e163aa8baf70aa25ad249ca7fb7518f959615321
Santiago Carmona <santiagocarmona911@gmail.com>
2024-02-21 17:47:11 -0500

    Initial commit

---
+++ src/main/java/com/jokeapi/gurru/GurruApplication.java
@@ -0,0 +1,13 @@
+package com.jokeapi.gurru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GurruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jokeapi/gurru/GurruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokeapi.gurru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GurruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
